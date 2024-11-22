a2a694286d35c3a4d17bdd6d2adda8403664d2dc
Sharshit24 <vermasumitharshit@gmail.com>
2024-11-22 12:32:24 +0530

    01vitereact file is added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+const username = "chai aur code"
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur code {username}</h1>
+    <p>lets test</p>
+  {/* <h1>Chai-aur-React-with-Vite|Hello Hrshuu</h1> */}
+  </>
+  )
+}
+
+export default App
